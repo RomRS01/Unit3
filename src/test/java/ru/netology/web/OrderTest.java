1816857d06c267539eebb51882cd97514bbfd8c5
Roman Rogachev <romanaddress@mail.ru>
2022-12-25 21:09:09 +0300

    Заливка тестов

---
+++ src/test/java/ru/netology/web/OrderTest.java
@@ -0,0 +1,24 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+class OrderTest {
+    @Test
+
+    void shouLdTest() {
+        open("http://localhost:9999");
+        // SelenideElement form = $("[data-test-id=callback-form]");
+        $("[data-test-id=name] input").setValue("Попов Василий");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+}
